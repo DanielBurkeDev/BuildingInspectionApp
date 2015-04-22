c830af8299bfb96ca310c7b980734274f6bf15df
Daniel Burke <danielburke.devstream@gmail.com>
2015-04-22 19:17:08 +0100

    Dan updated inspection events page

--- app/src/main/java/inspectplus/dpwgroup/com/inspectplus/SplashActivity.java
+++ app/src/main/java/inspectplus/dpwgroup/com/inspectplus/SplashActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class SplashActivity extends Activity {
@@ -21,20 +22,32 @@ public class SplashActivity extends Activity {
 
         etUsername = (EditText) findViewById(R.id.et_username);
         etPassword = (EditText) findViewById(R.id.et_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new ButtonListener());
 
-        Button btnLogin = (Button) findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
 
-                Intent intent = new Intent(SplashActivity.this, RegisteredProjectsActivity.class);
-                startActivity(intent);
 
+    }
+
+    private class ButtonListener implements View.OnClickListener {
+
+        public void onClick(View v) {
+            Intent intent;
+            switch (v.getId()) {
+                case R.id.btn_login:
+                    Toast.makeText(getApplicationContext(), "Login Button Selected",
+                            Toast.LENGTH_SHORT).show();
+                    intent = new Intent(SplashActivity.this, RegisteredProjectsActivity.class);
+                    startActivity(intent);
+                    break;
+
+                default:
+                    Toast.makeText(getApplicationContext(), "No options selected",
+                            Toast.LENGTH_SHORT).show();
             }
-        });
 
-
+        }
     }
 }
 
